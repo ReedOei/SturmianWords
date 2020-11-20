6d932bf570c8f2e4bdb660f5f52b702fe1250413
Reed Oei <oei.reed@gmail.com>
2020-11-19 18:20:58 -0600

    Add new theorem about antisquares/antipalindromes

--- test_implications.sh
+++ test_implications.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-rm -rf inclusion_tests_cleaned
 mkdir -p inclusion_tests_cleaned
 
 find "inclusion_tests/" -name "*_sub.aut" | while read -r fname; do
     other="$(echo "$fname" | sed -E "s/_sub/_sup/g")"
-    echo "Cleaning $fname and $other"
-    pecan --use-var-map="$fname" "$other" > temp
-    tail -n+2 "$fname" > "inclusion_tests_cleaned/$(basename "$fname")"
-    tail -n+2 "temp" > "inclusion_tests_cleaned/$(basename "$other")"
-    (
-        set -x
-        autfilt -c "inclusion_tests_cleaned/$(basename "$fname")" --included-in="inclusion_tests_cleaned/$(basename "$other")"
-    )
+    if [[ ! -f "inclusion_tests_cleaned/$(basename "$fname")" ]] ||
+       [[ ! -f "inclusion_tests_cleaned/$(basename "$other")" ]]; then
+        # echo "Cleaning $fname and $other"
+        (
+            set -x
+            pecan --heuristics --use-var-map="$fname" "$other" > temp
+        )
+        tail -n+2 "$fname" > "inclusion_tests_cleaned/$(basename "$fname")"
+        tail -n+1 "temp" > "inclusion_tests_cleaned/$(basename "$other")"
+    fi
 done
+
+find "inclusion_tests_cleaned/" -name "*_sub.aut" | while read -r fname; do
+    other="$(echo "$fname" | sed -E "s/_sub/_sup/g")"
+    ( set -x; autfilt -c "$fname" --included-in="$other" )
+done
+
 
