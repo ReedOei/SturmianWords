2ac662fe49e87b63b857e8475ba01350011ff5c6
Reed Oei <oei.reed@gmail.com>
2021-01-24 14:47:25 -0600

    Add the rest of the data

--- gather-data.sh
+++ gather-data.sh
@@ -11,21 +11,34 @@ fname="$2"
 # Clear the file
 > .pred_names
 
-cat "$name_map" | while read -r line; do
+cat "$name_map" | sort -n -k5 -t, | while read -r line; do
     display_name="$(echo "$line" | cut -f1 -d",")"
     name="$(echo "$line" | cut -f2 -d",")"
     should_display="$(echo "$line" | cut -f3 -d",")"
     complexity="$(echo "$line" | cut -f4 -d",")"
+    atoms="$(echo "$line" | cut -f5 -d",")"
 
     if [[ -z "$complexity" ]]; then
         complexity="\\text{\\reed{TODO}}"
     fi
 
+    if [[ -z "$atoms" ]]; then
+        atoms="\\text{\\reed{TODO}}"
+    fi
+
     if [[ "$should_display" == "true" ]]; then
-        runtime="$(printf %.2f "$(grep -w "Runtime for $name is" "$fname" | head -1 | sed -E "s/.*Runtime for (.*) is (.*)/\2/g")")"
-        states="$(grep -w "Max states for $name is" "$fname" | head -1 | sed -E "s/.*Max states for (.*) is (.*)/\2/g")"
-        edges="$(grep -w "Max edges for $name is" "$fname" | head -1 | sed -E "s/.*Max edges for (.*) is (.*)/\2/g")"
-        echo "$display_name & \$$complexity\$ & \$$runtime\$ & \$$states\$ & \$$edges\$ \\\\"
+        runtime="$(printf %.1f "$(grep -F "Runtime for $name is" "$fname" | head -1 | sed -E "s/.*Runtime for (.*) is (.*)/\2/g")")"
+        states="$(grep -F "Max states for $name is" "$fname" | head -1 | sed -E "s/.*Max states for (.*) is (.*)/\2/g")"
+        edges="$(grep -F "Max edges for $name is" "$fname" | head -1 | sed -E "s/.*Max edges for (.*) is (.*)/\2/g")"
+
+        if [[ -f "automata/$name.aut" ]]; then
+            stats="$(tail -n+2 "automata/$name.aut" | autfilt --stats="%s,%e")"
+            final_states="$(echo "$stats" | cut -f1 -d",")"
+            final_edges="$(echo "$stats" | cut -f2 -d",")"
+            echo "$display_name & \$$atoms\$ & \$$complexity\$ & \$$runtime\$ & \$$states\$ & \$$edges\$ & \$$final_states\$ & \$$final_edges\$ \\\\"
+        else
+            echo "$display_name & \$$atoms\$ & \$$complexity\$ & \$$runtime\$ & \$$states\$ & \$$edges\$ \\\\"
+        fi
     fi
 
     echo "$name" >> .pred_names
